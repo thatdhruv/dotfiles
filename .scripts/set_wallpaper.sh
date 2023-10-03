a5bb4e29d9d29db3722a44bb4afe199a5d8fe9d0
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2023-10-03 13:13:42 +0530

    Update set_wallpaper.sh

--- .scripts/set_wallpaper.sh
+++ .scripts/set_wallpaper.sh
@@ -3,4 +3,4 @@
 a=$((`ls -l ${HOME}/.wallpapers/ | wc -l`-1))
 b=${#a}
 
-feh --bg-scale "${HOME}/wallpapers/$(printf "%0$(echo $b)d" $(shuf -i 0-$a -n 1)).jpg"
+feh --bg-scale "${HOME}/.wallpapers/$(printf "%0$(echo $b)d" $(shuf -i 0-$a -n 1)).jpg"

3a0571033400314ad832f7b5f8d67ff5e2fb48bd
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2024-01-16 16:06:03 +0530

    Update screenshot.sh

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ -d "~/screenshots" ] ; then
-    scrot '%d%m%Y%H%M%S_$wx$h.png' -e 'mv $f ~/screenshots/ && notify-send "Screenshot saved!" "Screenshot saved as $f"'
+if [ -d "~/Screenshots" ] ; then
+    scrot '%d%m%Y%H%M%S_$wx$h.png' -e 'mv $f ~/Screenshots/ && notify-send "Screenshot saved!" "Screenshot saved as $f"'
 else
-    mkdir -p ~/screenshots
-    scrot '%d%m%Y%H%M%S_$wx$h.png' -e 'mv $f ~/screenshots/ && notify-send "Screenshot saved!" "Screenshot saved as $f"'
+    mkdir -p ~/Screenshots
+    scrot '%d%m%Y%H%M%S_$wx$h.png' -e 'mv $f ~/Screenshots/ && notify-send "Screenshot saved!" "Screenshot saved as $f"'
 fi
